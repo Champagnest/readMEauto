e8744877dae836d747dfc1594673ed9cf26ea2e7
Champagnest <mchampagned@protonmail.com>
2022-04-28 15:30:37 -0700

    added licenses

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,56 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+          if (titleInput) {
+            return true;
+          } else {
+            console.log('Please enter your project title!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description for your project (Required)',
+        validate: descriptionInput => {
+          if (descriptionInputt) {
+            return true;
+          } else {
+            console.log('Please enter your project description!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'tableOfContents',
+        message: 'Would you like to enter a table of contents?',
+        default: true
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide some information about yourself:',
+        when: ({ confirminstallation }) => confirminstallation
+      }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,38 +1,95 @@
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-var inquirer = require('inquirer');
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
+switch (license) {
+  case 'IBM':
+    return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)]'
+    break;
+
+    case 'ISC':
+    return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]'
+    break;
+
+    case 'MIT':
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]'
+    break;
+
+    case 'MPL':
+    return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]'
+    break;
+
+    case 'ODC':
+    return '[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)]'
+    break;
+
+    case 'perl':
+    return '[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)]'
+    break;
+
+    case 'unlicense':
+    return '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]'
+    break;
+}  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-
-
+  switch (license) {
+    case 'IBM':
+      return '(https://opensource.org/licenses/IPL-1.0)'
+      break;
+  
+      case 'ISC':
+      return '(https://opensource.org/licenses/ISC)'
+      break;
+  
+      case 'MIT':
+      return '(https://opensource.org/licenses/MIT)'
+      break;
+  
+      case 'MPL':
+      return '(https://opensource.org/licenses/MPL-2.0)'
+      break;
+  
+      case 'ODC':
+      return '(https://opendatacommons.org/licenses/by/)'
+      break;
+  
+      case 'perl':
+      return '(https://opensource.org/licenses/Artistic-2.0)'
+      break;
+  
+      case 'unlicense':
+      return '(http://unlicense.org/)'
+      break;
+  } 
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
-
+  if (license) {
+    return ` This application is using ${license} for licensing. `;
+  } else {
+    return '';
+  }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  *
 
 `;
 }
